60618340196a24db07a6bd52462b2c80b64b7694
root <root@localhost.localdomain>
2024-07-13 02:54:09 +0300

    Fixing Dockerfile problems

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import os
 
 app = Flask(__name__)
 
@@ -20,5 +21,6 @@ def login():
         return "Invalid credentials. Please try again."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
